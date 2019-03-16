5de9f6945868af60a255ad1ee632599ad58637f0
João Canavarro <jvcanavarro@gmail.com>
2019-03-16 17:37:56 -0300

    removing import csv

---
+++ iris_onlyPandas.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+
+
+def split_dataFrame(data):
+    pass
+
+
+def return_mmm():
+    pass
+
+
+def closet_row(index, dataset, show_range):
+    pass
+
+
+dataFrame = pd.read_csv("iris.csv.txt")
+
+print(dataFrame)
