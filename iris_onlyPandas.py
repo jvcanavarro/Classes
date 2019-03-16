484b45536f74e931cf72cba2fe4367047edcb487
João Canavarro <jvcanavarro@gmail.com>
2019-03-16 17:57:35 -0300

    .

--- iris_onlyPandas.py
+++ iris_onlyPandas.py
@@ -14,6 +14,13 @@ def closet_row(index, dataset, show_range):
     pass
 
 
+# opening iris data
 dataFrame = pd.read_csv("iris.csv.txt")
 
-print(dataFrame)
+# mean, max and min of each column
+print(dataFrame.mean(), "\n")
+print(dataFrame.iloc[:, dataFrame.columns != 'species'].max(), "\n")
+print(dataFrame.iloc[:, dataFrame.columns != 'species'].min(), "\n")
+
+# 10 rows with lower sum
+print()
