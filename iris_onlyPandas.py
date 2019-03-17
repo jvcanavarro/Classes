ac155aaf019f3c77884630ac04539acb63cecfc8
João Canavarro <jvcanavarro@gmail.com>
2019-03-17 11:34:14 -0300

    Finish 1st Part

--- iris_onlyPandas.py
+++ iris_onlyPandas.py
@@ -1,18 +1,11 @@
-import numpy as np
 import pandas as pd
+import numpy as np
 
 
-def split_dataFrame(data):
-    pass
-
-
-def return_mmm():
-    pass
-
-
-def closet_row(index, dataset, show_range):
-    pass
-
+def closest_row(index, dataset, show_range):
+    line = dataset.iloc[index, :-1].tolist()
+    dataset['eu_distance'] = np.linalg.norm(dataset.iloc[:, :-1].sub(line), axis=1)
+    return dataset.sort_values(['eu_distance']).iloc[1:show_range+1]
 
 # opening iris data
 dataFrame = pd.read_csv("iris.csv.txt")
@@ -23,4 +16,10 @@ print(dataFrame.iloc[:, dataFrame.columns != 'species'].max(), "\n")
 print(dataFrame.iloc[:, dataFrame.columns != 'species'].min(), "\n")
 
 # 10 rows with lower sum
-print()
+print(dataFrame.sum(axis=1, numeric_only=True).sort_values(ascending=True).head(10))
+
+# closest line(s) of nth line
+list_of_ranges = [5, 3, 1]
+for rnge in list_of_ranges:
+    print(closest_row(12, dataFrame, rnge))
+    dataFrame.drop('eu_distance', axis=1, inplace=True)
