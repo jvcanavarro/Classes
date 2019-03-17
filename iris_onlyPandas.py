fac7366a114265fd10da46f49845e1382872c9ae
João Canavarro <jvcanavarro@gmail.com>
2019-03-17 11:45:01 -0300

    Final Version Now (I guess)

--- iris_onlyPandas.py
+++ iris_onlyPandas.py
@@ -2,24 +2,26 @@ import pandas as pd
 import numpy as np
 
 
-def closest_row(index, dataset, show_range):
-    line = dataset.iloc[index, :-1].tolist()
-    dataset['eu_distance'] = np.linalg.norm(dataset.iloc[:, :-1].sub(line), axis=1)
-    return dataset.sort_values(['eu_distance']).iloc[1:show_range+1]
+def closest_row(index, dataFrame, show_range):
+    line = dataFrame.iloc[index, :-1].tolist()
+    dataFrame['eu_distance'] = np.linalg.norm(
+        dataFrame.iloc[:, :-1].sub(line), axis=1)
+    return dataFrame.sort_values(['eu_distance']).iloc[1:show_range+1]
+
 
 # opening iris data
 dataFrame = pd.read_csv("iris.csv.txt")
 
 # mean, max and min of each column
 print(dataFrame.mean(), "\n")
-print(dataFrame.iloc[:, dataFrame.columns != 'species'].max(), "\n")
-print(dataFrame.iloc[:, dataFrame.columns != 'species'].min(), "\n")
+print(dataFrame.iloc[:, :-1].max(), "\n")
+print(dataFrame.iloc[:, :-1].min(), "\n")
 
 # 10 rows with lower sum
-print(dataFrame.sum(axis=1, numeric_only=True).sort_values(ascending=True).head(10))
+print(dataFrame.sum(axis=1, numeric_only=True).sort_values(ascending=True).head(10), '\n')
 
 # closest line(s) of nth line
 list_of_ranges = [5, 3, 1]
 for rnge in list_of_ranges:
-    print(closest_row(12, dataFrame, rnge))
+    print(closest_row(12, dataFrame, rnge), '\n')
     dataFrame.drop('eu_distance', axis=1, inplace=True)
