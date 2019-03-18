122e131a4c63021d1abef87b2ed66b5d288a18bb
João Canavarro <jvcanavarro@gmail.com>
2019-03-18 09:11:01 -0300

    Robust Code

--- iris_onlyPandas.py
+++ iris_onlyPandas.py
@@ -2,26 +2,26 @@ import pandas as pd
 import numpy as np
 
 # Part 1: Data Basic Operations
-def closest_row(index, dataFrame, line = None,  show_range = 1, rang = False):
+
+def closest_rows(index, dataFrame, line=None,  num_of_rows=1, multLines=False):
+    best_matching_rows = []
     if not line:
         line = dataFrame.iloc[index].tolist()
     dataFrame['euc_distance'] = np.linalg.norm(
         dataFrame.iloc[:, :-1].sub(line[:-1]), axis=1)
-    if rang:
-        return dataFrame.sort_values(['euc_distance']).iloc[1:show_range+1]
-    return dataFrame.sort_values(['euc_distance']).iloc[0, -2]
+    best_matching_rows =  dataFrame.sort_values(['euc_distance']).iloc[0, -2]
+    if multLines:
+        best_matching_rows = dataFrame.sort_values(['euc_distance']).iloc[1:num_of_rows+1]
+    dataFrame.drop('euc_distance', axis=1 , inplace=True)
+    return best_matching_rows
 
 
-def make_euclidian_prediction(dataFrame):
+def make_euclidian_prediction(test_data, train_data):
     right_predictions = 0
-    train_data, test_data = dataFrame[:100], dataFrame[100:].reset_index(drop=True)
-    for i in range(len(test_data.index)):
+    for i in range(len(test_data.index)):   # iterate throw all test_data rows
         row_of_test = test_data.iloc[i].tolist()
-        # train_data['euc_distance'] = np.linalg.norm(
-        #    train_data.iloc[:, :-1].sub(row_of_test[:-1]), axis=1)
-        if closest_row(i, train_data, row_of_test) == row_of_test[-1]:
+        if closest_rows(i, train_data, row_of_test) == row_of_test[-1]:
             right_predictions += 1
-        train_data.drop('euc_distance', axis=1, inplace=True)
     return right_predictions
 
 
@@ -39,9 +39,8 @@ print(dataFrame.sum(axis=1, numeric_only=True).sort_values(
 
 # closest line(s) of nth line (d, e ,f)
 list_of_ranges = [5, 3, 1]
-for rnge in list_of_ranges:
-    print(closest_row(12, dataFrame, None, rnge, True), '\n')
-    dataFrame.drop('euc_distance', axis=1, inplace=True)
+for show_range in list_of_ranges:
+    print(closest_rows(12, dataFrame, None, show_range, True), '\n')
 
 # Part 2: Predicting Species
 # 1st Task - Mix Data
@@ -53,6 +52,6 @@ train_data, test_data = dataFrame[:100], dataFrame[100:].reset_index(drop=True)
 # 3rd Task - Make Predictions Based on Euc. Distance
 pd.options.mode.chained_assignment = None
 print("Number of right predictions:",
-      make_euclidian_prediction(dataFrame))
+      make_euclidian_prediction(test_data, train_data))
 
 # 4th Task - Make Predictions Based on Sin.
