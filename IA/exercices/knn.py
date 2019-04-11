d4abcacd2fca43a4b8cb37d08cfa80b52f897af7
jvcanavarro <jvcanavarro@gmail.com>
2019-04-11 11:50:36 -0300

    Added Plot to knn.py

--- IA/exercices/knn.py
+++ IA/exercices/knn.py
@@ -1,4 +1,5 @@
-﻿import pandas as pd
+﻿import matplotlib.pyplot as plt
+import pandas as pd
 import numpy as np
 #import math
 #from decimal import Decimal
@@ -58,9 +59,7 @@ def randomize_data(dataFrame):
 
 
 def check_right_prediction(possible_predictions, answer):
-    if answer in possible_predictions:
-        return True
-    return False
+    return answer in possible_predictions
 
 
 def count_predictions(test_data, train_data, num_of_rows, euclidian_prediction=True):
@@ -72,20 +71,19 @@ def count_predictions(test_data, train_data, num_of_rows, euclidian_prediction=T
     return right_predictions
 
 
-def calculate_final_results(test_data, train_data, k, euc_distance = True):
+def calculate_final_results(test_data, train_data, k, file_name, euc_distance = True):
     hits = count_predictions(test_data, train_data, k, euc_distance)
     misses = len(test_data) - hits
-    accuracy = hits / len(test_data) * 100   
-    return round(accuracy, 2), hits, misses, 0, 0, 0, euc_distance, k
+    accuracy = hits / len(test_data) * 100
+    if euc_distance:
+        return round(accuracy, 2), hits, misses, 'euclidian', file_name, len(test_data), k
+    return round(accuracy, 2), hits, misses, 'cosine', file_name, len(test_data), k
 
 
 pd.options.mode.chained_assignment = None
-files = ['iris.csv', 'glass.csv', 'diabetes.csv']
-columns = ['acc', 'hits', 'misses', 'abserror_mean', 'std_deviation', 'confusion_matrix', 'euclidian_distance', 'KNN']
+files = ['iris.csv', 'glass.csv']
 knn = [1, 3]
 
-
-results = pd.DataFrame(columns=columns)
 list_results = [] 
 for file_name in files:
     print('\nProcessing...')
@@ -93,10 +91,33 @@ for file_name in files:
     dataFrame = pd.read_csv(file_name)
     train_data, test_data = randomize_data(dataFrame)
     for k in knn:
-        list_results.append(list(calculate_final_results(test_data, train_data, k)))
+        list_results.append(list(calculate_final_results(test_data, train_data, file_name, k)))
         train_data, test_data = randomize_data(dataFrame)
-        list_results.append(list(calculate_final_results(test_data, train_data, k, False)))
-    print('-'*80)
+        list_results.append(list(calculate_final_results(test_data, train_data, file_name, k, False)))
 
+print('-'*80)
+
+columns = ['prec', 'hits', 'misses', 'distance', 'KNN', 'test_length', 'file']
 results = pd.DataFrame(data=list_results, columns=columns)
 print(results)
+print('\nStandard Deviation:', round(results['prec'].std(), 2))
+print('Mean Absolute Error:', round(results['misses'].mean(), 2))
+print('Mean Absolute Deviation', round(results['prec'].mad(), 2))
+print('Accuracy:', round(results['prec'].mean(), 2), '%')
+# TODO: Confusion Matrix
+
+# iris(k1,k3)-glass(k1,k3)-diabetes(k1,k3)
+weka_precision = [96.0784,96.0784,67.1233,63.0137,72.7969,74.7126]
+weka_error = [100 - x for x in weka_precision]
+x = np.arange(len(weka_precision))
+width = 0.6
+plt.figure()
+prec = plt.bar(x, weka_precision, width, color='c')
+error = plt.bar(x, weka_error, width, color='r')
+plt.xlabel('Datasets')
+plt.ylabel('Precision')
+plt.title('Weka')
+plt.xticks(x, ('iris k1', 'iris  k3', 'glass k1', 'glass k3', 'diab. k1', 'diab. k3'))
+# plt.grid(linestyle='--', color='k')
+plt.legend((prec[0], error[0]), ('Precision', 'Error'))
+plt.show()
