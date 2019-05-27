775bfcd425477e19bb790f9970e7ffda72270990
jvcanavarro <jvcanavarro@gmail.com>
2019-05-27 16:26:20 -0300

    .

--- Graphs/digraph.py
+++ Graphs/digraph.py
@@ -1,44 +1,58 @@
-
+# João Victor da Silva Dias Canavarro - 201704940015
+from collections import defaultdict
 
 class Digraph(object): # Adj. List
+    """
+    A class used to represent a directed graph using adjacency list
+    ...
+    Atributes
+    ---------
+    self.digraph organized as nested dictionaries
+    {node1: {neighbor1: weight1, neighbor2: weight2}, node2: {}, ...}
+    Example:
+        self.digraph = {1: {1, 2, 3}, 2: {3}, 3: {1: 0}}
+    self.edges organized as a list of tuples
+    [(vertice1 -> vertice2), (vertice5 -> vertice2), (vertice3 -> vertice3), ...]
+    Example:
+        self.edges = [(1, 1), (1, 2), (1, 3), (2, 3), (3, 1), (4, 1)]
 
+    Methods
+    -------
+
+    """
     def __init__(self, digraph: dict = {}) -> None:
-
-        # self.digraph = digraph
-        self.digraph = {node: {weight:0 for weight in neighbors} for node, neighbors in digraph.items()}
-        print(self.digraph)
+        # self.digraph = {node: {weight:0 for weight in neighbors} for node, neighbors in digraph.items()}
+        self.digraph = defaultdict(dict, digraph)
 
     def get_vertices(self) -> set:
         return set(self.digraph.keys())
 
     def get_edges(self) -> list:
-        return self.create_edges()
+        self.edges = [(vertice, neighbor) for vertice in self.digraph for neighbor in self.digraph[vertice]]
+        # equal to:
+        # for vertice in self.digraph:
+        #     for neighbor in self.digraph[vertice]:
+        #             edges.append((vertice, neighbor))
+        return self.edges
 
-    def set_edges(self, edge: tuple, weight: int = 0) -> None:
+    def set_edges(self, edge: tuple) -> None:
+        # edge = (vertice1 -> vertice2)
         if edge[0] in self.digraph:
-            self.digraph[edge[0]].append(edge[1])
+            self.digraph[edge[0]].add(edge[1])
         else:
-            self.digraph[edge[0]] = [edge[1]]
-
-        self.digraph[edge[0][1]] = weight
-        print(self.digraph)
-
-    def vertice_exists(self):
-        pass
-
-    def create_edges(self) -> list:
-        pass
-
+            self.digraph[edge[0]] = {edge[1]}
 
 # graph = {1:{}, 2:{}}
-graph = {1: {1, 2, 3}, 2: {3}, 3: {1:0}}
-graph[3].update({2:0})
-print(graph)
+graph = {1: {1, 2, 3}, 2: {3}, 3: {1: 0}}
+
 digraph = Digraph(graph)
 
-edge = (2, 1)
-# digraph.set_edges(edge, 2)
-# print(digrafo.get_vertices())
+# edge = (2, 1, 0)
+# edge = {2: {1: 0}}
 
+edge = (4, 1)
+digraph.set_edges(edge)
+# digraph.create_edges()
+print(digraph.get_edges())
+print(help(digraph))
 
-# https://codereview.stackexchange.com/questions/163414/adjacency-list-graph-representation-on-python
