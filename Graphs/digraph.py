1651b96bb9f21aaad3fa83bc43f49132d6ad2622
jvcanavarro <jvcanavarro@gmail.com>
2019-05-27 12:25:59 -0300

    Working on Digraph

--- Graphs/digraph.py
+++ Graphs/digraph.py
@@ -1,9 +1,11 @@
 
 
-class Digraph(object):
+class Digraph(object): # Adj. List
 
     def __init__(self, digraph: dict = {}) -> None:
-        self.digraph = digraph
+
+        # self.digraph = digraph
+        self.digraph = {node: {weight:0 for weight in neighbors} for node, neighbors in digraph.items()}
         print(self.digraph)
 
     def get_vertices(self) -> set:
@@ -12,39 +14,31 @@ class Digraph(object):
     def get_edges(self) -> list:
         return self.create_edges()
 
-    def set_vertice(self, vertice: int) -> None:
-        if vertice not in self.digraph:
-            self.digraph[vertice] = []
+    def set_edges(self, edge: tuple, weight: int = 0) -> None:
+        if edge[0] in self.digraph:
+            self.digraph[edge[0]].append(edge[1])
+        else:
+            self.digraph[edge[0]] = [edge[1]]
 
-    def set_edges(self, edge) -> None:
-        edge = set(edge)  # Set = Conjunto de elementos únicos.
-        vertice = edge.pop()
-        if edge:
-            vertice2 = edge.pop()
-        else:
-            vertice2 = vertice
-        if vertice in self.digraph:
-            self.digraph[vertice].append(vertice2)
-        else:
-            self.digraph[vertice] = [vertice2]
+        self.digraph[edge[0][1]] = weight
+        print(self.digraph)
+
+    def vertice_exists(self):
+        pass
 
     def create_edges(self) -> list:
         pass
 
 
-graph = {1: {1, 2, 3}, 2: {3}, 3: {1}}
+# graph = {1:{}, 2:{}}
+graph = {1: {1, 2, 3}, 2: {3}, 3: {1:0}}
+graph[3].update({2:0})
+print(graph)
+digraph = Digraph(graph)
 
-# for key, value in grafo.items():
-# 	print('key', key)
-# 	print('value', value)
-# 	graph[key] = {el:0 for el in value}
-
-
-digrafo = Digraph(graph)
-
-digrafo.set_vertice(4)
-
-print(digrafo.get_vertices())
+edge = (2, 1)
+# digraph.set_edges(edge, 2)
+# print(digrafo.get_vertices())
 
 
 # https://codereview.stackexchange.com/questions/163414/adjacency-list-graph-representation-on-python
