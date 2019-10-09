ef5b343805d353b021bc3bf45c05f743a6bf9377
jvcanavarro <jvcanavarro@gmail.com>
2019-10-08 22:57:34 -0300

    One Time Pad for Sentences

--- Security/onetime_pad.py
+++ Security/onetime_pad.py
@@ -1,38 +1,27 @@
 import numpy as np
-import typing
 import click
 
 
 def generate_key(length):
-    key = np.random.randint(2, size=length)
-    return key
+    return np.random.randint(2, size=length)
 
 
 def encrypt_message(message, key):
-    xor = []
-    # print(message ^ ?!?jedi=0, key)?!? (*_*o: object=...*_*) ?!?jedi?!?
-    for m_bit, k_bit i?!?jedi=0, n zip(message, key):?!? (*_*o: bytes*_*, encoding: str=..., errors: str=...) ?!?jedi?!?
-        print(bool (str(m_bit) ^ bool (k_bit))
-        if m_bit == k_bit:
-            xor.append(0)
-        else:
-            xor.append(1)
-    return xor
+    return [int(m_bit) ^ k_bit for m_bit, k_bit in zip(message, key)]
 
 
 @click.command()
 @click.option('-m', 'message')
-def onetime_pad(message):
+@click.option('-s', 'sentence', is_flag=True)
+def onetime_pad(message, sentence):
     key = generate_key(len(message))
-
-    print(message)
-    print(key)
+    print(list(key), '| Key')
 
     enc_message = encrypt_message(message, key)
-    print('Encrypted Message:', enc_message)
+    print(enc_message, '| Encrypted Message')
 
-    dec_message = encrypt_message(message, key)
-    print('Decrypted Message:', dec_message)
+    dec_message = encrypt_message(enc_message, key)
+    print(dec_message, '| Decrypted Message')
 
 
 if __name__ == '__main__':
