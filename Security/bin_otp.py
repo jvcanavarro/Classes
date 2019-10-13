b08e4fc3f24733edc50204279acb85fe61e87e71
jvcanavarro <jvcanavarro@gmail.com>
2019-10-12 21:50:09 -0300

    One Time Pad for Files

--- Security/bin_otp.py
+++ Security/bin_otp.py
@@ -2,10 +2,6 @@ import numpy as np
 import click
 
 
-def bit_to_string(bits):
-    return ''.join(chr(int(''.join(bit), 2)) for bit in zip(*[iter(bits)]*7))
-
-
 def generate_key(length):
     return np.random.randint(2, size=length)
 
@@ -14,38 +10,34 @@ def encrypt_message(message, key):
     return [int(m_bit) ^ k_bit for m_bit, k_bit in zip(message, key)]
 
 
-def binarize_sentence(sentence):
-    sentence = map(bin, bytearray(sentence, 'utf8'))
-    return ''.join(bit[2:] for bit in sentence)
+def text_to_bits(text, encoding='utf-8'):
+    bits = bin(int.from_bytes(text.encode(encoding), 'big'))[2:]
+    return bits.zfill(8 * ((len(bits) + 7) // 8))
 
 
-def message_to_string(sentence):
-    sentence = list(map(str, sentence))
-    sentence = [''.join(sentence[i:i + 8]) for i in range(0, len(sentence), 7)]
-    return ''.join(map(bit_to_string, sentence))
+def text_from_bits(bits, encoding='utf-8'):
+    n = int(''.join(map(str, bits)), 2)
+    return n.to_bytes((n.bit_length() + 7) // 8, 'big').decode(encoding)
 
 
 @click.command()
-@click.option('-m', 'message')
-@click.option('-s', 'sentence', is_flag=True)
-def onetime_pad(message, sentence):
-    if sentence:
-        message = binarize_sentence(message)
+@click.option('-m', 'message', required=True)
+def onetime_pad(message):
 
-    key = generate_key(len(message))
-    print(list(key), '| Key')
+    enc_message = []
+    dec_message = []
 
-    enc_message = encrypt_message(message, key)
-    print(enc_message, '| Encrypted Message')
+    for word in message.split():
+        word = text_to_bits(word)
+        key = generate_key(len(word))
 
-    if sentence:
-        print(message_to_string(enc_message))
+        enc_message.append(encrypt_message(word, key))
+        dec_message.append(encrypt_message(enc_message[-1], key))
 
-    dec_message = encrypt_message(enc_message, key)
-    print(dec_message, '| Decrypted Message')
+        dec_message[-1] = text_from_bits(dec_message[-1])
 
-    if sentence:
-        print(message_to_string(dec_message))
+        print(enc_message)
+        print(dec_message[-1], '\n')
 
 
 if __name__ == '__main__':
--- Security/str_otp.py
+++ Security/str_otp.py
@@ -1,4 +1,4 @@
-import operator
+from operator import sub, add
 import string
 import random
 import click
@@ -16,35 +16,32 @@ def get_char_by_index(index):
     return string.ascii_lowercase[index]
 
 
-def encrypt_message(message, key):
+def encrypt_message(message, key, func, decrypt=False):
     index_message = map(get_index_of_char, message)
     index_key = map(get_index_of_char, key)
 
-    enc_message = map(sum, zip(index_message, index_key))
-    return map(lambda x: x % 26, enc_message)
+    enc_message = map(func, index_message, index_key)
+    enc_message = map(lambda x: x % 26, enc_message)
 
-def decrypt_message(message, key):
-    index_key = map(get_index_of_char, key)
-
-    dec_message = map(operator.sub, message, index_key)
-    dec_message = map(lambda x: x % 26, dec_message)
-
-    return ''.join(map(get_char_by_index, dec_message))
+    return ''.join(map(get_char_by_index, enc_message))
 
 
 @click.command()
-@click.option('-m', 'message', required=True)
-@click.option('-k', 'key', default='')
+@click.option('-m', 'message', help='Message', required=True)
+@click.option('-k', 'key', help='Key',  default='')
 def onetime_pad(message, key):
     if len(key) != len(message):
         print('Missing Key or Text/Key size is different.')
         print('Generating a Random Key.\n')
         key = generate_key(len(message))
 
-    enc_message = encrypt_message(message, key)
-    dec_message = decrypt_message(enc_message, key)
-    print(enc_message)
-    print(dec_message)
+    enc_message = encrypt_message(message, key, add)
+    dec_message = encrypt_message(enc_message, key, sub, True)
+
+    print('Original Message:  ', message)
+    print('Encrypted Message: ', enc_message)
+    print('Decrypted Message: ', dec_message)
+
 
 if __name__ == '__main__':
     onetime_pad()
