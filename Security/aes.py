dedc8db66355fc4a75af40d0b02c5d4bfd41283b
jvcanavarro <jvcanavarro@gmail.com>
2019-10-17 14:51:47 -0300

    Missing PBK

---
+++ Security/aes.py
@@ -0,0 +1,81 @@
+from collections import Counter
+from Crypto.Cipher import AES, DES3
+from functools import reduce
+from Crypto import Random
+from operator import add
+import click
+
+
+def pad_data(data):
+    if len(data) % 16 == 0:
+        return data
+    databytes = bytearray(data)
+    padding_required = 15 - (len(databytes) % 16)
+    databytes.extend(b'\x80')
+    databytes.extend(b'\x00' * padding_required)
+    return bytes(databytes)
+
+
+def unpad_data(data):
+    if not data:
+        return data
+
+    data = data.rstrip(b'\x00')
+    if data[-1] == 128:
+        return data[:-1]
+    else:
+        return data
+
+
+def generate_aes_key(des3):
+    if des3:
+        rnd = Random.OSRNG.posix.new().read(24)
+    else:
+        rnd = Random.OSRNG.posix.new().read(AES.block_size)
+    return rnd
+
+
+def encrypt(key, iv, data, des3):
+    if des3:
+        enc = DES3.new(key, DES3.MODE_ECB, iv)
+    else:
+        enc = AES.new(key, AES.MODE_ECB, iv)
+    data = pad_data(data)
+    return enc.encrypt(data)
+
+
+def decrypt(key, iv, data, des3):
+    if des3:
+        dec = DES3.new(key, DES3.MODE_ECB, iv)
+    else:
+        dec = AES.new(key, AES.MODE_ECB, iv)
+    data = dec.decrypt(data)
+    return unpad_data(data)
+
+
+@click.command()
+@click.option('--des3', is_flag=True)
+def test_crypto(des3):
+    msg = b"This is some super secret message.  Please don't tell anyone about it or I'll have to shoot you."
+    key = generate_aes_key(des3)
+
+    iv1 = b"12345678"
+    iv2 = b"12345679"
+
+    code1 = encrypt(key, iv1, msg, des3)
+    code2 = encrypt(key, iv2, msg, des3)
+
+    # decoded1 = decrypt(key, iv1, code1)
+    # decoded2 = decrypt(key, iv2, code2)
+
+    occ_bits = map(lambda x: Counter(bin(x)[2:]), code1)
+    sum_bits1 = reduce(add, occ_bits)
+    print(sum_bits1)
+
+    occ_bits = map(lambda x: Counter(bin(x)[2:]), code2)
+    sum_bits2 = reduce(add, occ_bits)
+    print(sum_bits2)
+
+
+if __name__ == '__main__':
+    test_crypto()
--- Security/bin_otp.py
+++ Security/bin_otp.py
@@ -1,16 +1,16 @@
-import Crypto.Random
-import numpy as np
+from Crypto import Random
 import click
 
 
 def generate_key(length):
-    a = Crypto.Random.get_random_bytes(length)
-    print(a)
-    return Crypto.Random.get_random_bytes(length)
-    # return np.random.randint(2, size=length)
+    return Random.get_random_bytes(length)
 
 
 def encrypt_message(message, key):
+    # for k, m in zip(message[:8], key[:8]):
+    #     print('k', k)
+    #     print('m', m)
+    #     print(int(k) ^ m)
     return [int(m_bit) ^ k_bit for m_bit, k_bit in zip(message, key)]
 
 
@@ -31,26 +31,17 @@ def onetime_pad(file, text):
 
     if file:
         text = file.read()
-        print(text.split())
 
-    enc_message = []
-    dec_message = []
+    message = text_to_bits(text)
+    key = generate_key(len(message))
 
+    enc_msg = encrypt_message(message, key)
+    print('Encrypted Message:', enc_msg)
 
-    for word in text.split():
-        word = text_to_bits(word)
-        key = generate_key(len(word))
+    dec_msg = encrypt_message(enc_msg, key)
+    dec_msg = text_from_bits(dec_msg)
 
-        enc_message.append(encrypt_message(word, key))
-        dec_message.append(encrypt_message(enc_message[-1], key))
-
-        dec_message[-1] = text_from_bits(dec_message[-1])
-
-        # print(enc_message)
-        # print(dec_message[-1], '\n')
-
-    print("Encrypted Message: ", enc_message)
-    print("Decrypted Message: ",  ' '.join(dec_message))
+    print('Decrypted Message:', dec_msg)
 
 
 if __name__ == '__main__':
